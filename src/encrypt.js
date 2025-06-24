91d69b18a5da3de6e50545af40249f51559098a6
gromber05 <gromber2911@g.educaand.es>
2025-06-24 09:30:32 +0200

    refactor: remove unused decrypt function and related documentation from encrypt.js

--- src/encrypt.js
+++ src/encrypt.js
@@ -10,16 +10,6 @@ function encrypt(password) {
   return bcrypt.hashSync(password, 10);
 }
 
-/**
- * Encrypts the given password.
- * 
- * @param {string} password - Generated password to encrypt.
- * @returns {string} - The encrypted password
- */
-function decrypt(password) {
-  return bcrypt.decodeBase64(password, 10);
-}
-
 module.exports = {
     encrypt,
     decrypt
